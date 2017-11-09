4d4ad5a3c8b69de16939fc7fd099b185d7e63a86
coderTomato <570547651@qq.com>
2017-11-09 14:09:14 +0800

    添加图片

--- QQMusicDemo/QQMusicDemo/Classes/Other/DataTool/QQMusicDataTool.swift
+++ QQMusicDemo/QQMusicDemo/Classes/Other/DataTool/QQMusicDataTool.swift
@@ -9,5 +9,27 @@
 import UIKit
 
 class QQMusicDataTool: NSObject {
+    
+    class func getMusicModels(models result: ([QQMusicModel])->()){
+        //1.获取文件路径
+        guard let path = Bundle.main.path(forResource: "Musics.plist", ofType: nil) else{
+            result([QQMusicModel]());
+            return ;
+        }
+        //2.读取文件内容
+        guard let array = NSArray(contentsOfFile: path) else{
+            result([QQMusicModel]());
+            return;
+        }
+        //3.解析：转换成歌曲模型
+        var models = [QQMusicModel]();
+        for dict in array{
+            let model = QQMusicModel(dic:dict as! [String : AnyObject])
+            models.append(model)
+        }
+        
+        //4.返回结果
+        result(models)
+    }
 
 }
--- QQMusicDemo/QQMusicDemo/Classes/QQMain/Model/QQMusicModel.swift
+++ QQMusicDemo/QQMusicDemo/Classes/QQMain/Model/QQMusicModel.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class QQMusicModel: NSObject {
-
+    
+    /** 歌曲名称 */
+    var name: String?
+    /** 歌曲文件名称 */
+    var filename: String?
+    /** 歌词文件名称 */
+    var lrcname: String?
+    /** 歌手名称 */
+    var singer: String?
+    /** 歌手头像名称 */
+    var singerIcon: String?
+    /** 专辑头像图片 */
+    var icon: String?
+    
+    override init(){
+        super.init()
+    }
+    
+    init(dic: [String : AnyObject]){
+        super.init();
+        setValuesForKeys(dic);
+    }
 }
